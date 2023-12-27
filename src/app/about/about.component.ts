a273550008eb622e9c74b560296bba95544c0a17
apla12 <anapaulalopes12@gmail.com>
2023-12-27 22:45:54 +0000

    add project

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { faThinkPeaks } from '@fortawesome/free-brands-svg-icons';
+import { faAnchor, faChess, faPenRuler, faPencilRuler, faShare } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {
+  pencil = faPenRuler;
+  strategy = faChess;
+  anchor = faAnchor;
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
+import { StoreComponent } from './store/store.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductComponent,
+    ClientComponent,
+    ContactComponent,
+    StoreComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [FormsModule, BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

65ed1d75c626cb5b3ec7341312cb1a42f2651739
apla12 <anapaulalopes12@gmail.com>
2024-01-16 11:07:44 +0000

    new tag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ProductComponent } from './product/product.component';
-import { ClientComponent } from './client/client.component';
-import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [
-  { path: 'home', component: AppComponent },
-
-];
+const routes: Routes = [{ path: 'home', component: AppComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
